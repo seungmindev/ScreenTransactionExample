59e53352aee3be4c964a84510489778653e84441
seungmin <seungmindev@github.com>
2022-04-09 12:15:53 +0900

    Initial Commit

---
+++ ScreenTransactionExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenTransactionExample
+//
+//  Created by seungminlee on 2022/04/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
